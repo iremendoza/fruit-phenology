3f4b024c1251531247a1b6b5d44e78c453659495
Irene Mendoza <irene.mendoza.sagrera@gmail.com>
2016-07-13 10:01:29 -0300

    bubble plot function added

--- fruit phenology Neotrop-Mendoza-functions.R
+++ fruit phenology Neotrop-Mendoza-functions.R
@@ -17,3 +17,21 @@ draw.tropics = function(color = "grey", ltype = 2, lwidth = 0, ...){
   lines(c(151, 180), c(-23.5, -23.5), col = color, lty = ltype, lwd = lwidth)
 }
 
+####bubble.plot####
+#bubble plot is copied from R book for illustrating variation across different locations in the x-y plane
+bubble.plot<-function(xv,yv,rv,bs=0.1,maint,...)
+{
+  r<-rv/max(rv)
+  yscale<-max(yv,na.rm=T)-min(yv,na.rm=T)
+  xscale<-max(xv,na.rm=T)-min(xv,na.rm=T)
+  plot(xv,yv,type="n",main=maint,...)
+  for (i in 1:length(xv)) bubble(xv[i],yv[i],r[i],bs,xscale,yscale) }
+
+bubble<-function (x,y,r,bubble.size,xscale,yscale) 
+{
+  theta<-seq(0,2*pi,pi/200)
+  yv<-r*sin(theta)*bubble.size*yscale
+  xv<-r*cos(theta)* bubble.size*xscale
+  lines(x+xv,y+yv) 
+}
+
